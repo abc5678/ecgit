3461c42072cd082b813de0518ae61cae0e0edc6e
hong <hong@naver.com>
2019-06-19 16:24:08 +0900

    sample class 추가

---
+++ src/webgit1/Sample.java
@@ -0,0 +1,22 @@
+package webgit1;
+
+public class Sample {
+	private String name;
+	private String age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	
+
+}
